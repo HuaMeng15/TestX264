b4e3a199079e4febe69abdf2a98f3d72c5f9537b
HuaMeng <hmengab@connect.ust.hk>
2025-08-28 14:36:36 +0800

    Add x265 infos.

---
+++ test_x265.cpp
@@ -0,0 +1,328 @@
+#include "stdint.h"
+#include "x265.h"
+#include "x265_config.h"
+#include <iostream>
+#include <filesystem>
+#include <string.h>
+// Time Analysis
+#include <chrono>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+#define QP 21 // Used when CQP
+#define PRESET "superfast"
+
+const bool ENABLE_LOG = true;
+const string FILE_PREFIX = "/Users/menghua/Research/TestX264/";
+const string VIDEO_PREFIX = "/Users/menghua/Downloads/VideoSources/";
+const string DIFFERENCE_DIR = "/Users/menghua/Research/TestX264/difference/";
+const double REDUCE_RATIO = 0.8;
+
+void InitEncodeParam(x265_param &param, int initial_bitrate, int frame_rate, int width, int height, double vbv_buffer_size, int qp_step) {
+    x265_param_default_preset(&param, PRESET, "zerolatency");
+    param.sourceWidth = width;
+    param.sourceHeight = height;
+    param.fpsNum = frame_rate;
+    param.fpsDenom = 1;
+    // param.internalBitDepth = 8;
+    param.internalCsp = X265_CSP_I420;
+    param.logLevel = X265_LOG_DEBUG;
+    param.frameNumThreads=1;
+    param.rc.rateControlMode= X265_RC_ABR;
+    param.rc.bitrate = initial_bitrate * REDUCE_RATIO;
+    param.rc.vbvMaxBitrate = param.rc.bitrate;
+    param.rc.vbvBufferSize = param.rc.bitrate * vbv_buffer_size;
+    param.bframes = 0;
+    param.bOpenGOP = 0;
+    // param.bConfigRCFrame = 1;
+
+    /* Configure non-default params */
+    // param.b_sliced_threads = 1;
+    // param.i_bitdepth = 8;
+
+    // param.b_vfr_input = 0;
+
+    // param.b_cabac = 1;  // 0 for CAVLC， 1 for higher complexity
+}
+
+struct BitrateConfig {
+    int start_frame_index;
+    int bitrate;
+};
+
+void ReadBitrateConfig(FILE *bitrate_file, vector<BitrateConfig> &bitrate_config_vec) {
+    BitrateConfig config;
+    while (fscanf(bitrate_file, "%d, %d", &config.start_frame_index,
+                  &config.bitrate) != EOF) {
+        // config.bitrate = config.bitrate / 2;
+        bitrate_config_vec.push_back(config);
+        if (config.start_frame_index == 0) {
+            BitrateConfig config2 = config;
+            config2.start_frame_index = 2; // avoid head I frame effect
+            bitrate_config_vec.push_back(config2);
+        }
+    }
+    if (ENABLE_LOG) {
+        cout << "Read " << bitrate_config_vec.size() << " bitrate config" << endl;
+        for (int i = 0; i < bitrate_config_vec.size(); i++) {
+            cout << "start_frame_index:" << bitrate_config_vec[i].start_frame_index << " bitrate:" << bitrate_config_vec[i].bitrate << endl;
+        }
+    }
+}
+
+void UpdateBitrateConfig(x265_encoder *encoder, x265_param &param, int bitrate, double vbv_buffer_size) {
+    bitrate = bitrate * REDUCE_RATIO;
+    cout << "Reconfig to bitrate:" << bitrate << " vbv_buffer_size:" << vbv_buffer_size << endl;
+    param.rc.bitrate = bitrate;
+    param.rc.vbvMaxBitrate = bitrate; // * 10;
+    param.rc.vbvBufferSize = bitrate * vbv_buffer_size;
+
+    int ret = x265_encoder_reconfig(encoder, &param);
+    cout << "Reconfig return: " << ret << endl;
+}
+
+int WriteNALToFile(FILE *file_out, x265_nal *nal, int i_nal) {
+    if (i_nal > 0) {
+        for (int i = 0; i < i_nal; i++) {
+            size_t written = fwrite(nal[i].payload, 1, nal[i].sizeBytes, file_out);
+            if (written != nal[i].sizeBytes) {
+                perror("Failed to write data to file");
+                fclose(file_out);
+                return -1;
+            }
+        }
+    } else if (i_nal < 0) {
+        return -1;
+    }
+    return 0;
+}
+
+const string GenerateVideoFilename(const string &video_name) {
+    size_t pos = video_name.find("Game");
+    if (pos != std::string::npos) {
+        return VIDEO_PREFIX + "Game/" + video_name + ".yuv";
+    } else {
+        return VIDEO_PREFIX + video_name + ".yuv";
+    }
+}
+
+std::vector<double> GetVideoDifference(const string &video_name) {
+    std::vector<double> video_difference;
+    string difference_file = DIFFERENCE_DIR + video_name + "_mse.log";
+    cout << "Reading video difference from: " << difference_file << endl;
+    // exit(0);
+    if (!std::filesystem::exists(difference_file)) {
+        cout << "Difference file does not exist: " << difference_file << endl;
+        return video_difference;
+    }
+    std::ifstream infile(difference_file);
+    double value;
+    while (infile >> value) {
+        video_difference.push_back(value);
+    }
+    infile.close();
+    return video_difference;
+}
+
+bool float_equal(float a, float b, float epsilon = 1e-5f) {
+    return std::abs(a - b) < epsilon;
+}
+
+void EncodeAndGenerateStatistics(const string &video_name, const string &bitrate_config_filename, const string &output_dir, int initial_bitrate, int frame_rate, int width, int height, double vbv_buffer_size, int response_time, const string &suffix, int qp_step, double dropped_vbv_buffer_size, int reduce_number) {
+    x265_param param;
+    x265_encoder *encoder;
+    x265_picture* pic = x265_picture_alloc();
+    x265_picture* pic_out = x265_picture_alloc();
+    int i_frame = 0;
+    int i_frame_size;
+    x265_nal *nal;
+    uint32_t i_nal;
+    int current_bitrate = 0;
+
+    string send_log_file = output_dir + "send.log";
+    string bitrate_config_file =  FILE_PREFIX + "input/bitrate_config/" + bitrate_config_filename;
+    string input_video_file = GenerateVideoFilename(video_name);
+    cout << "input_video_file: " << input_video_file << endl;
+    string output_video_file = output_dir + "result.mkv";
+
+    // std::vector<double> video_differences = GetVideoDifference(video_name);
+
+    if (!std::filesystem::is_directory(output_dir)) {
+        cout << "Create directory: " <<  output_dir << endl;
+        std::filesystem::create_directory(output_dir);
+    }
+
+    FILE *input_yuv_file = fopen(input_video_file.c_str(), "rb");
+    FILE *encoded_file_out = fopen(output_video_file.c_str(), "wb");
+    FILE *bitrate_file = fopen(bitrate_config_file.c_str(), "rb");
+    std::ofstream send_file(send_log_file);
+    if (!input_yuv_file || !encoded_file_out || !bitrate_file) {
+        cout << "fopen failed input_yuv_file:" << !input_yuv_file << " encoded_file_out:" << !encoded_file_out << " bitrate_file: " << !bitrate_file << endl;
+        return;
+    }
+
+    vector<BitrateConfig> bitrate_config_vec;
+    ReadBitrateConfig(bitrate_file, bitrate_config_vec);
+
+    current_bitrate = bitrate_config_vec[0].bitrate;
+    int bitrate_config_index = 1;
+    int drop_period_frames = 0;
+    double current_vbv_buffer_size = vbv_buffer_size;
+
+    InitEncodeParam(param, current_bitrate, frame_rate, width, height, vbv_buffer_size, qp_step);
+
+    // cout << "param i_bitrate: " << param.rc.i_bitrate << " max_bitrate: " << param.rc.i_vbv_max_bitrate << " vbv_buffer_size: " << param.rc.i_vbv_buffer_size << endl;
+
+    if (!pic || !pic_out) {
+        cout << "x265_picture_alloc failed" << endl;
+        return;
+    }
+    x265_picture_init(&param, pic);
+    cout << "pic.width: " << pic->width << " pic.height: " << pic->height << endl;
+
+    encoder = x265_encoder_open(&param);
+    if( !encoder ) return;
+
+    int luma_size = param.sourceWidth * param.sourceHeight;
+    int chroma_size = luma_size / 4;
+
+    int enable_drop = 0;
+
+    /* Encode frames */
+    for(;; i_frame++) {
+        // if (i_frame > 0) {
+        //     cout << "Processing frame: " << i_frame << " Frame difference: " << (i_frame - 1 < video_differences.size() ? video_differences[i_frame - 1] : 0) << endl;
+        // }
+        // Update bitrate
+        if (bitrate_config_index < bitrate_config_vec.size()) {
+            if (i_frame == bitrate_config_vec[bitrate_config_index].start_frame_index + response_time) {
+                current_bitrate = bitrate_config_vec[bitrate_config_index].bitrate;
+                double vbv_ratio = vbv_buffer_size;
+                if (suffix == "_adaptive_") {
+                    cout << "bitrate_config_index: " << bitrate_config_index << " current_bitrate: " << current_bitrate << endl;
+                    if (bitrate_config_index > 1) {
+                        // Reduce vbv_buffer_size for reduce_number frames
+                        drop_period_frames = reduce_number;
+                        vbv_ratio = dropped_vbv_buffer_size;
+                        cout << "Update drop_period_frames: " << drop_period_frames << endl;
+                    }
+                }
+                if (bitrate_config_index > 1) {
+                    // enable_drop = reduce_number;
+                    cout << "Enable drop frames for " << enable_drop << " frames" << endl;
+                }
+                current_vbv_buffer_size = vbv_ratio;
+                UpdateBitrateConfig(encoder, param, current_bitrate, vbv_ratio);
+                // cout << "Update bitrate at frame" << i_frame << " Current bitrate: " << current_bitrate << " codec bitrate: " << param.rc.i_bitrate << " vbv_buffer_size: " << param.rc.i_vbv_buffer_size << endl;
+                bitrate_config_index++;
+            }
+        }
+        if (drop_period_frames > 0) {
+            drop_period_frames--;
+            if (drop_period_frames == 0) {
+                UpdateBitrateConfig(encoder, param, current_bitrate, vbv_buffer_size);
+                current_vbv_buffer_size = vbv_buffer_size;
+                // cout << "Update bitrate at frame" << i_frame << " Current bitrate: " << current_bitrate << " codec bitrate: " << param.rc.i_bitrate << " vbv_buffer_size: " << param.rc.i_vbv_buffer_size << endl;
+            }
+        }
+        /* Read input frame */
+        auto encode_start = chrono::high_resolution_clock::now();
+
+        char *buff = NULL;
+        buff=(char *)malloc(luma_size * 3 / 2);
+        pic->planes[0] = buff;
+        pic->planes[1] = buff + luma_size;
+        pic->planes[2] = buff + luma_size * 5 / 4;
+        pic->stride[0] = param.sourceWidth;
+        pic->stride[1] = param.sourceWidth / 2;
+        pic->stride[2] = param.sourceWidth / 2;
+        if (fread(pic->planes[0], 1, luma_size, input_yuv_file) != (unsigned)luma_size) break;
+        if (fread(pic->planes[1], 1, chroma_size, input_yuv_file) != (unsigned)chroma_size) break;
+        if (fread(pic->planes[2], 1, chroma_size, input_yuv_file) != (unsigned)chroma_size) break;
+
+        if (enable_drop > 0) {
+            if (i_frame > 4 && i_frame % 4 != 0) {
+                cout << "Drop frame: " << i_frame << endl;
+                auto now = std::chrono::high_resolution_clock::now();
+                // Convert to milliseconds since epoch
+                auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+                send_file << ms << " Encode frame:" << i_frame + 1 << " frame_size:0" << std::endl;
+                enable_drop--;
+                continue; // Drop frame
+            }
+            // UpdateBitrateConfig(encoder, param, current_bitrate, vbv_ratio);
+        }
+
+        pic->pts = i_frame;
+
+        x265_encoder_encode(encoder, &nal, &i_nal, pic, NULL);
+        i_frame_size = 0;
+        for (int i = 0; i < i_nal; i++) {
+            i_frame_size += nal[i].sizeBytes;
+        }
+        if (WriteNALToFile(encoded_file_out, nal, i_nal) < 0) {
+            cout << "WriteNALToFile failed" << endl;
+            return;
+        }
+        cout << "Write frame: " << i_frame + 1 << " nal type: " << nal->type << " size: " << i_frame_size <<
+        " size_kbps: " << i_frame_size * 8 * 30 / 1000 << " kbps" << endl;
+
+        auto encode_end = chrono::high_resolution_clock::now();
+        auto encode_duration = chrono::duration_cast<chrono::milliseconds>(encode_end - encode_start);
+        int encoded_duration = encode_duration.count();
+
+        auto now = std::chrono::high_resolution_clock::now();
+        // Convert to milliseconds since epoch
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+        send_file << ms << " Encode frame:" << i_frame + 1 << " frame_size:" << i_frame_size << std::endl;
+    }
+
+    /* Flush delayed frames */
+    // while (x264_encoder_delayed_frames(encoder)) {
+    //     if (WriteNALToFile(encoded_file_out, nal, i_frame_size) < 0) {
+    //         cout << "WriteNALToFile failed" << endl;
+    //         return;
+    //     }
+    //     cout << "Write delayed frame: " << i_frame + 1 << " nal type: " << nal->i_type << " size: " << i_frame_size << endl;
+    // }
+
+    x265_encoder_close(encoder);
+    x265_picture_free(pic);
+
+    fclose(input_yuv_file);
+    fclose(encoded_file_out);
+    fclose(bitrate_file);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 9) {
+        std::cerr << "Invalid arguments number:  " << argc << ", expected 5" << std::endl;
+        return 1; // Return an error code
+    }
+    string video_name = argv[1];
+    string bitrate_filename = argv[2];
+    char* end;
+    double vbv_buffer_size = std::strtod(argv[3], &end);
+    string output_dir = argv[4];
+    int response_time = 1;//std::strtod(argv[5], &end);
+    string suffix = "_adaptive_";//"_";//argv[6];
+    suffix = "_";//argv[6];
+    int frame_rate = std::strtod(argv[5], &end);
+    int qp_step = std::strtod(argv[6], &end);
+    double dropped_vbv_buffer_size = std::strtod(argv[7], &end);
+    int reduce_number = std::strtod(argv[8], &end);
+    cout << "bitrate_filename:" << bitrate_filename << " vbv_buffer_size:" << vbv_buffer_size << endl;
+    cout << "output_dir:" << output_dir << " response_time:" << response_time << " frame_rate:" << frame_rate << " qp_step:" << qp_step << " dropped_vbv_buffer_size:" << dropped_vbv_buffer_size << " reduce_number:" << reduce_number << endl;
+    // return 0;
+
+    // Basic configurations
+    int initial_bitrate = 10000;
+    int width = 1920;
+    int height = 1080;
+
+    EncodeAndGenerateStatistics(video_name, bitrate_filename, output_dir, initial_bitrate, frame_rate, width, height, vbv_buffer_size, response_time, suffix, qp_step, dropped_vbv_buffer_size, reduce_number);
+
+    return 0;
+}
